566af0bbbd81df8dcd280da7316bfd7e18cf0e98
asiagromasia <35482401+asiagromasia@users.noreply.github.com>
2020-06-02 22:56:28 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,58 +1,92 @@
-import React from 'react';
-import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import React, { Component, useEffect } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
-//import { Col, Button, Form, FormGroup, Label, Input } from "reactstrap";
+//import { Col, Button, Form, FormGroup, Label, Input } from 'reactstrap';
+// Redux
+import { Provider } from 'react-redux';
+import store from './store';
+import { loadUser } from './actions/auth';
+import setAuthToken from './utils/setAuthToken';
+
+// Components
 import Landing from './components/Landing.component';
-import Login from "./components/Login.component";
-import LoginProvider from "./components/LoginProvider.component";
-import Navbar from "./components/Nbar.component";
-import CreateSymptom from "./components/Create-patient-symptom.component";
-import PatientProfile from "./components/PatientProfile.component";
+import LoginPatient from './components/auth/LoginPatient.component';
+import RegisterPatient from './components/auth/RegisterPatient.component';
+import LoginProvider from './components/auth/LoginProvider.component';
+import RegisterProvider from './components/auth/RegisterProvider.component';
+import Navbar from './components/Nbar.component';
+import CreateSymptom from './components/Create-patient-symptom.component';
+import PatientProfile from './components/PatientProfile.component';
 import WelcomeProvider from './components/WelcomeProvider.component';
 import PatientSearch from './components/PatientSearch.component';
 import PatientAlertList from './components/PatientAlertList.component';
 import PatientImmediateAttList from './components/PatientImmediateAttList.component';
 
+// check for token
+if (localStorage.token) {
+  setAuthToken(localStorage.token);
+}
 
 function App() {
+  useEffect(() => {
+    store.dispatch(loadUser());
+  }, []);
+
   return (
-    <Router>
-      <div>
-        <div className="container">
-          <br/>
-          <Route path="/">
+    <Provider store={store}>
+      <Router>
+        <div className='mainBody'>
+          <div className='container'>
+            <br />
+            <Route exact path='/'>
               <Landing />
-          </Route>
-        </div>
-      <div className="container">
-          <Route path="/patient/login">
-              <Login />
-          </Route>
-       </div>
-          <Route path="/provider/login">
+            </Route>
+          </div>
+          <div className='container'>
+            <Route exact path='/patient/login'>
+              <LoginPatient />
+            </Route>
+          </div>
+          <div className='container'>
+            <Route exact path='/patient/register'>
+              <RegisterPatient />
+            </Route>
+          </div>
+          <div className='container'>
+            <Route exact path='/provider/login'>
               <LoginProvider />
-          </Route>
-         <Route path="/patient/:id">
+            </Route>
+          </div>
+          <div className='container'>
+            <Route exact path='/provider/register'>
+              <RegisterProvider />
+            </Route>
+          </div>
+          <div className='container'>
+            <Route exact path='/patient/login/:id'>
               <Navbar />
               <CreateSymptom />
-          </Route>
-        <Route path="/patient/:id/profile">
+            </Route>
+          </div>
+          <div className='container'>
+            <Route exact path='/patient/:id/profile'>
               <Navbar />
               <PatientProfile />
-          </Route>
-         <Route path="/provider/:id">
+            </Route>
+          </div>
+          <div className='container'>
+            <Route exact path='/provider/login/:id'>
               <Navbar />
               <WelcomeProvider />
               <PatientSearch />
               <PatientAlertList />
               <PatientImmediateAttList />
-          </Route>
-            
+            </Route>
+          </div>
         </div>
-     
-    </Router>
+      </Router>
+    </Provider>
   );
 }
-
 export default App;
